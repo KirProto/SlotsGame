76743f22f3ebef2762ae111efbef4c25bef14667
Kir Proto Q <kir.proto@ya.ru>
2022-02-27 09:14:26 +0500

    Initial Commit

---
+++ Chris_SlotsGame/Chris_SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chris_SlotsGameApp.swift
+//  Chris_SlotsGame
+//
+//  Created by KIR Q on 2022.02.27.
+//
+
+import SwiftUI
+
+@main
+struct Chris_SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
